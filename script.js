e1d8e223e2d068a215b05f774f1b917a03c9ec5c
ThakareSakshi <sakshithakare121@gmail.com>
2023-10-25 00:51:43 +0530

    updated ui

---
+++ script.js
@@ -0,0 +1,7 @@
+// start--------------->
+
+let payment_section=document.querySelector(".payment");
+let pay_btn=document.querySelector(".pay_btn")
+pay_btn.addEventListener("click",()=>{
+   payment_section.innerHTML=""
+})
