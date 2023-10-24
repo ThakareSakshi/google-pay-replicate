9771faa95a466ef7c6443299cb1b677753fead8b
ThakareSakshi <sakshithakare121@gmail.com>
2023-10-25 01:17:53 +0530

    added multiple pages

--- script.js
+++ script.js
@@ -3,5 +3,20 @@
 let payment_section=document.querySelector(".payment");
 let pay_btn=document.querySelector(".pay_btn")
 pay_btn.addEventListener("click",()=>{
-   payment_section.innerHTML=""
+   payment_section.innerHTML=`
+   <header>
+        <input type="text" class="mobile_number" placeholder="Enter 10 digit mobile number">
+
+        
+        
+
+    </header>
+    <main class="">
+        
+    </main>
+    <footer class="page_2_footer">
+        <button class="proceed_btn">Procced</button>
+    </footer>`
 })
+
+// second page---------------->
