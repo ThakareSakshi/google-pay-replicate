8d7a420dda6f01e94169def44371c3cec0047420
ThakareSakshi <sakshithakare121@gmail.com>
2023-10-25 15:49:26 +0530

    updated

--- script.js
+++ script.js
@@ -3,14 +3,15 @@
 let mainPage=document.querySelector(".main_page")
 let numberpage=document.querySelector(".number_page")
 let AmountPage=document.querySelector(".amount_page")
-let PinPage=document.querySelector(".Pin_page")
-let finalpaymentPage=document.querySelector(".final_payment")
+let PinPage=document.querySelector(".pin_page")
+let finalpaymentPage=document.querySelector(".successful")
 let errorPage=document.querySelector(".error_page")
 
 // -----------fetching all btns----------------->
 const pay_btn=document.querySelector(".pay_btn");
 const proceed_btn=document.querySelector(".proceed_btn")
 const pay_amount_btn=document.querySelector("#pay_amount_btn")
+const final_payment_btn=document.querySelector("#final_payment")
 
 
 
@@ -51,3 +52,52 @@ proceed_btn.addEventListener("click",()=>{
 })
 
 //----------------add amount---->
+
+let Pay_amount=document.querySelector(".Pay_amount");
+
+Pay_amount.addEventListener("input",()=>{
+    pay_amount_btn.innerText="pay "+"₹ "+Pay_amount.value
+})
+
+pay_amount_btn.addEventListener("click",()=>{
+    let validate_payment=new Promise((resolve,reject)=>{
+        if(Pay_amount.value >=10000){
+            reject("max transfer limit is 9,999 rs. only");
+        }else{
+            resolve(Pay_amount.value)
+        }
+    })
+
+    validate_payment.then((amount)=>{
+
+      PinPage.style.display="block";
+      AmountPage.style.display="none";
+      document.querySelector(".pin_page .paying_to_number").innerText=document.querySelector(".paying_to_number").innerText;
+
+
+    })
+})
+
+// ------------------validate pin----------------->
+
+final_payment_btn.addEventListener("click",()=>{
+    let validate_pin=new Promise((resolve,reject)=>{
+        let pin=document.getElementById("pin");
+        if(pin.value.length<6){
+            reject("wrong pin");
+           }
+           else{
+            resolve(Pay_amount.value);
+           }
+    })
+
+    validate_pin.then(()=>{
+        setTimeout(()=>{
+            
+            finalpaymentPage.style.display="block";
+        },3000);
+    }).catch((msg)=>{
+        console.log(msg);
+    })
+})
+
